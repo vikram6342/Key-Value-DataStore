f40a1e87567002706b653cedfc7444ff69d07034
Vikram Jayanth <95023888+vikram6342@users.noreply.github.com>
2023-01-19 20:08:49 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+
+
+
+#file structure - "key","value"
+
+
+def search(file_header,key : Str) ->int : # vikram
+	"""
+	returns the positon to be seeked
+
+	-1 if element not found 
+	"""
+	pass
+
+
+
+def insert(file_header,key: Str, value: Str) -> None: #dinesh
+
+	"""
+	Inserts the contents to the file in the format
+	"key","value"
+	key -> 32 characters (len)
+	value -> 1mb (len(2^20))
+	Insetion should update when key exist	
+
+	"""
+  pass
+
+
+def delete(file_header,key: Str) -> None: #jayson
+	"""
+	Removes the row if key exist
+	if not exist Should say key not exist
+	"""
+  pass
+
+def find_value(file_header, key: Str) -> None #Hari
+	"""
+	Prints the value 
+	if value does not exists print key not found!
+ 	"""
+  pass
+
+
+#Hari -> file obj create 
+#			Menu driven program 
+
+#1 -> insert
+#2 -> delete
+#key ->32 value -> 1mb
+
+#Insert/ Update
+#Delete
+#Find 
+
+#f = open("filename.txt", "r+")
